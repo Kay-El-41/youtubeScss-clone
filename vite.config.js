c4964a730be06e110a4f171eb5478cff883728cf
Angie Hong <lsuvhyc1@gmail.com>
2025-02-16 23:02:12 +0800

    vercel.json update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1500, // Increase limit from 500kB to 1500kB
+  server: {
+    port: 3000, // Vercel will override this
   },
-})
+  build: {
+    outDir: "dist", // Make sure this matches Vercel's expectations
+  },
+});
