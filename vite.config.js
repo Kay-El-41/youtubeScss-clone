db00210ca0eda3f236d670fa0c48d3aa7e7c9bf2
Angie Hong <lsuvhyc1@gmail.com>
2025-02-16 23:14:34 +0800

    base  update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",  // Set this to "/" for root deployment
   plugins: [react()],
   server: {
     port: 3000, // Vercel will override this
